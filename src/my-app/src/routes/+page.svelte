1c2dc2061a3b317495677fad1ce86633c11d7527
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2024-01-06 09:29:44 +0530

    created svelte page to display blogposts through api

---
+++ src/my-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+
+    let caldata = "";
+    onMount (async ()=> {
+        const fetchData = await fetch("http://localhost:3000/coal")
+        fetchData.json().then(data => {
+            caldata = data["data"]
+            console.log(caldata)
+        })
+
+    })
+
+    
+</script>
+
+{#if caldata}
+{#each caldata as data }
+    <h1>{data["post_id"]}. {data["title"]}</h1>
+    <p>{data["content"]}</p>
+{/each}
+
+{/if}
